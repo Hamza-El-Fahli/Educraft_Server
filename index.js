326aa21983f6ad70865d1852d995c2e9185a337e
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-02-09 16:06:16 +0100

    wip: use Firestore Cloud

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-const express = require('express')
-const myJSON = require('./lib')
-const port = 3000;
+import { query, collection , where, getDocs } from "firebase/firestore";
+import { db } from "./firestore.js";
 
 
-const app = express()
-const users=new myJSON()
-app.get('/',async (req,res)=>{
-    res.send('Hiiii')
-})
-app.get('/allusers',async (req,res)=>{
-    res.json(await users.data)
-})
+const docRef = query(collection(db,'utilisateurs'));
+const querySnapshot = await getDocs(docRef);
 
-app.get('/adduser/:email/:password/:name',async (req,res)=>{
-    const newUser = {
-        name : req.params.name,
-        email : req.params.email,
-        password : req.params.password,
-    }
-    await users.write(newUser)
-    res.json(newUser.name)
-})
+try{
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+  });
+} catch(e) {
 
-app.get('/isuser/:email/:password/',async (req,res)=>{
-    const newUser = {
-        email : req.params.email,
-        password : req.params.password,
-    }
-    const result = await users.find(newUser)
-    res.json(result)
-})
-
-
-app.listen(port,()=>{console.log(`listening at http://localhost:${port}`)})
+    console.log("No such document! ",e);
+}
