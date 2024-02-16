f8d4ad46e8217b67a48fafc60ebd44d100a1ed47
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-02-16 21:41:36 +0100

    wip: add template , and loading

--- src/app/(authentification)/(with-auth-layout)/template.tsx
+++ src/app/(authentification)/(with-auth-layout)/template.tsx
@@ -3,16 +3,32 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import "./styles.css";
+import { useState } from "react";
+
 const navLinks: { name: string; href: string }[] = [
   { name: "Login", href: "/login" },
   { name: "Register", href: "/register" },
   { name: "Forgot-Password", href: "/forgot-password" },
 ];
 
-export default function AuthLayout({ children }: any) {
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const currentPath = usePathname();
+
+  const [input, setinput] = useState("");
+
   return (
     <>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setinput(e.target.value)}
+        />
+      </div>
       {navLinks.map((nav) => {
         const isActive = currentPath.startsWith(nav.href);
 
