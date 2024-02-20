992d06a18d91b0f4df858c9757434b6f07078c9a
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-02-20 18:55:45 +0100

    wip:dashboard basic structure to dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,11 @@
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="border grid grid-cols-12 grid-rows-12 h-screen text-white text-4xl">
+      <div className="border h-full w-full bg-gray-800 col-span-12">header</div>
+      <nav className="border h-full w-full bg-gray-800 col-span-2 row-span-10">nav</nav>
+      <main className="border h-full w-full bg-gray-800 col-span-10 row-span-10">main</main>
+
+      <footer className="border h-full w-full bg-gray-800 col-span-12">footer</footer>
+    </div>
+  );
 }
