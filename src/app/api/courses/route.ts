21678df09f643f35e76610dbc349b4765af64816
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-02-26 09:10:18 +0100

    wip: courses api
    -import db connection
    -import courses schema

---
+++ src/app/api/courses/route.ts
@@ -0,0 +1,24 @@
+import connectDB from "@/database/lib/mongodb";
+import Courses from "@/database/models/courses"
+import { NextRequest, NextResponse } from "next/server";
+
+
+
+
+// PUT      add one course
+// GET      select all courses
+// POST     select one course 
+// DELETE   delete a course by id
+
+
+export async function PUT(request : Request) {
+    const {course_name, description, instructor} = await request.json()
+    try {
+        await connectDB()
+    await Courses.create({course_name , description , instructor})
+    return NextResponse.json({message:"Course created successfuly"},{status : 201})
+    } catch (error) {
+        
+        return NextResponse.json({error:"No courses created" , contexst : error})
+    }
+}
