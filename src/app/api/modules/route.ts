0e73662c53dfb18b3635c52f4b98f24b339ef127
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-02-27 11:30:55 +0100

    wip: modules API
    - create GET method to get moduules
    - add filtering method

--- src/app/api/modules/route.ts
+++ src/app/api/modules/route.ts
@@ -16,3 +16,24 @@ export async function POST(request : NextRequest){
 
 }
 
+
+// GET      select modules with specific course_id
+// note : if no course_id, select all
+export async function GET(request: NextRequest) {
+    try {
+        await connectDB();
+        const  course_id  = await request.nextUrl.searchParams.get('course_id');
+        console.log(course_id)
+        let filter: any = {};
+        if (course_id) {
+            filter["course_id"] = course_id;
+        }
+        const res = await _Modules.find(filter);
+        if (res.length === 0 && course_id) {
+            return NextResponse.json({ error: "Course_id not found" }, { status: 404 });
+        }
+        return NextResponse.json(res);
+    } catch (error) {
+        return NextResponse.json({ error: "No Modules were found", context: error }, { status: 404 });
+    }
+}
