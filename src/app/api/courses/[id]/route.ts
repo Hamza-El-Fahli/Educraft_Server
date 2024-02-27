e1b89df25096c1a973c70bc6583340b0916d5d03
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-02-27 09:51:42 +0100

    wip:courses API
    -add update courses method

--- src/app/api/courses/[id]/route.ts
+++ src/app/api/courses/[id]/route.ts
@@ -7,13 +7,25 @@ import connectDB from "@/database/lib/mongodb";
 
 export async function POST(request:NextRequest,{params}:{params:{id:string}}){
     const {id} = params
+    const { new_course_name,new_description,new_instructor} =await request.json()
     
     try {
-        const { new_course_name,new_description,new_instructor} =await request.json()
         await connectDB()
-        await Courses.findByIdAndUpdate(id,{course_name : new_course_name , description : new_description , instructor : new_instructor})
-        return NextResponse.json({message : "Course updated successfuly"},{status : 201})
+        const res = await Courses.findByIdAndUpdate(id,{course_name : new_course_name , description : new_description , instructor : new_instructor})
+        return NextResponse.json({message : "Course updated successfuly" , data : res},{status : 201})
     } catch (error) {
         return NextResponse.json({message : "No course updated" , error:error})
     }
+}
+
+export async function DELETE(request:NextRequest,{params}:{params:{id : string}}){
+    const {id} = params
+    try {
+        await connectDB()
+        const res = await Courses.findByIdAndDelete(id)
+        return NextResponse.json({message : "Course deleted successfuly", data : res})
+    } catch (error) {
+        return NextResponse.json({message : "No course deleted" , error : error})
+    }
+
 }
