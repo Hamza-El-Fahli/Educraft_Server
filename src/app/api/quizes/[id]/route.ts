12834269f248462bd09cf4992d7ae56c6a016884
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-03-02 01:19:03 +0100

    wip:  Readme file
    - add Quiz CRUD guide

--- src/app/api/quizes/[id]/route.ts
+++ src/app/api/quizes/[id]/route.ts
@@ -8,30 +8,30 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 
-export async function PUT(request:NextRequest, {params}:{params:{id : string}}){
-    const {id} = params
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params
     try {
         await connectDB()
-        const { new_question ,  new_chapter_id , new_correct_answer , new_options} = await request.json()
-        console.log( new_question ,  new_chapter_id , new_correct_answer , new_options)
+        const { new_question, new_chapter_id, new_correct_answer, new_options } = await request.json()
+        console.log(new_question, new_chapter_id, new_correct_answer, new_options)
 
-        await Quizes.findByIdAndUpdate(id ,{question:new_question ,chapter_id:new_chapter_id ,correct_answer:new_correct_answer, options : new_options} )
-        return NextResponse.json({message:"Quize updated successfuly"})
+        await Quizes.findByIdAndUpdate(id, { question: new_question, chapter_id: new_chapter_id, correct_answer: new_correct_answer, options: new_options })
+        return NextResponse.json({ message: "Quize updated successfuly" })
     } catch (error) {
-        return NextResponse.json({message:"No Quizes were updated", context:error})
+        return NextResponse.json({ message: "No Quizes were updated", context: error })
     }
 }
 
 
 
-export async function DELETE(request:NextRequest, {params}:{params:{id : string}}){
-    const {id} = params
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params
     try {
         await connectDB()
         await Quizes.findByIdAndDelete(id)
-        return NextResponse.json({message:"Quize Deleted successfuly"})
+        return NextResponse.json({ message: "Quize Deleted successfuly" })
     } catch (error) {
-        return NextResponse.json({message:"No Quizes were deleted", context:error})
+        return NextResponse.json({ message: "No Quizes were deleted", context: error })
     }
 }
 
--- src/database/models/quizes.ts
+++ src/database/models/quizes.ts
@@ -1,23 +1,23 @@
-import mongoose,{Model,Document, Schema} from "mongoose";
+import mongoose, { Model, Document, Schema } from "mongoose";
 
 
 interface IQuiz extends Document {
-    chapter_id : string;
-    question : string;
-    correct_answer : number;
-    options : string[];
+    chapter_id: string;
+    question: string;
+    correct_answer: number;
+    options: string[];
 }
 
 
 const QuizSchema = new Schema<IQuiz>({
-    
+
     chapter_id: { type: String, required: true },
     question: { type: String, required: true },
     correct_answer: { type: Number, required: true },
-    options: { type: [String] , required: true },
+    options: { type: [String], required: true },
 })
 
-const Quizes  : Model<IQuiz>= mongoose.models.Quizes as Model<IQuiz>  || mongoose.model('Quizes',QuizSchema)
+const Quizes: Model<IQuiz> = mongoose.models.Quizes as Model<IQuiz> || mongoose.model('Quizes', QuizSchema)
 
 
 export default Quizes
