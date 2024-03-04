8cf85260f506e7a124739432004383f668e8ddc9
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-03-04 19:19:19 +0100

    wip: courses managements
    - fix Update course working

--- src/app/(dashboard)/courses/page.tsx
+++ src/app/(dashboard)/courses/page.tsx
@@ -17,7 +17,8 @@ interface Course {
 export default function Courses() {
   const [isOpen, setIsOpen] = useState(false);
   const [Loading, setLoading] = useState(true);
-  var AddTrueORmodifyFalse = true 
+  var AddTrueORmodifyFalse = true;
+  const [AddORMod, setAddORMod] = useState(true);
   // temporary fixing a bug
   // when modify the requent send to AddUser first then modify
   const openModal = () => {
@@ -32,28 +33,26 @@ export default function Courses() {
     axios.get(`${URL_Server}`).then(
       (res) => {
         setCourses(res.data);
-        setLoading(false)
+        setLoading(false);
       },
       (rej) => {
         alert(rej);
       }
     );
   }, []);
-  
-  
+
   const handleSubmit = (e: any) => {
-    if(!AddTrueORmodifyFalse) return
-    const frm = e.target;
+    const frm = document.querySelector("form");
     e.preventDefault();
     const tmp = {
-      course_name: frm.Coursename.value,
-      description: frm.description.value,
-      instructor: frm.instructor.value
+      course_name: frm?.Coursename.value,
+      description: frm?.description.value,
+      instructor: frm?.instructor.value,
     };
-    console.log(tmp)
+    console.log(tmp);
     axios.post(`${URL_Server}`, tmp).then(
       (res) => {
-        setCourses([...Courses,{_id : res.data._id, ...tmp}]);
+        setCourses([...Courses, { _id: res.data._id, ...tmp }]);
 
         closeModal();
       },
@@ -64,60 +63,63 @@ export default function Courses() {
   };
 
   async function modifyCourse(e: any) {
-    AddTrueORmodifyFalse = false
     const tds = e.target.parentNode.parentNode.querySelectorAll("td");
     await openModal();
-
     const form: any = document.querySelector("form");
     const inputs = form?.querySelectorAll("input");
     for (let td = 1; td <= inputs.length; td++) {
       inputs[td - 1].value = tds[td].innerText;
-      // tds[td].innerText = '';
-      // tds[td].appendChild(input);
     }
 
-    form.addEventListener("submit", (e: any) => {
-
+    document.querySelector("#send")?.addEventListener("click", () => {
+      console.log(tds[0].textContent.trim());
       e.preventDefault();
-      axios.put(`${URL_Server}/${tds[0].textContent.trim()}`, {
-        course_name: inputs[0].value,
-        description: inputs[1].value,
-        instructor: inputs[2].value
-    }).then((res) => {
-        for (let td = 0; td < inputs.length; td++) {
+      axios
+        .put(`${URL_Server}/${tds[0].textContent.trim()}`, {
+          course_name: inputs[0].value,
+          description: inputs[1].value,
+          instructor: inputs[2].value,
+        })
+        .then((res) => {
+          for (let td = 0; td < inputs.length; td++) {
             tds[td + 1].innerText = inputs[td].value;
-        }
-        closeModal();
-    }).catch((error) => {
-        alert('Error updating course');
-        closeModal();
+          }
+          closeModal();
+        })
+        .catch((error) => {
+          alert("Error updating course");
+          closeModal();
+        });
     });
-    
-    });
-    AddTrueORmodifyFalse = true
   }
 
   async function removeCourse(e: any) {
-    const tds = e.target.parentNode.parentNode.children
+    const tds = e.target.parentNode.parentNode.children;
     const id = tds[0].textContent;
-    const decision = window.confirm(`Are you sure to delete course ${tds[1].textContent}`)
+    const decision = window.confirm(
+      `Are you sure to delete course ${tds[1].textContent}`
+    );
     const newState = Courses.filter((user) => user._id != id);
-    if(decision)
-    axios.delete(`${URL_Server}/${id}`).then((res)=>{
-      setCourses(newState);
-      alert(res.data.message)
-    },()=>{
-      alert('Error')
-    })
+    if (decision)
+      axios.delete(`${URL_Server}/${id}`).then(
+        (res) => {
+          setCourses(newState);
+          alert(res.data.message);
+        },
+        () => {
+          alert("Error");
+        }
+      );
   }
 
-  return ( <div className="dashboardContainer">
+  return (
+    <div className="dashboardContainer">
       {/*  */}
       <Modal isOpen={isOpen} onClose={closeModal}>
         <h2 className="text-lg font-bold mb-2 text-blue-800">Add User</h2>
         <p className="mb-4 text-blue-400">Fill the form</p>
         <form
-          onSubmit={(e) => {e.preventDefault();handleSubmit(e)}}
+          onSubmit={(e) => e.preventDefault()}
           className="flex flex-col gap-3 w-80 "
         >
           <input
@@ -141,10 +143,26 @@ export default function Courses() {
             name="instructor"
             placeholder="instructor"
           />
-          <button         
- type="submit" className="text-primary h-12 border p-3">
-            Save
-          </button>
+
+          {AddORMod ? (
+            <button
+              type="submit"
+              onClick={(e) => {
+                handleSubmit(e);
+              }}
+              className="text-primary h-12 border p-3"
+            >
+              Save
+            </button>
+          ) : (
+            <button
+              type="submit"
+              id="send"
+              className="text-primary h-12 border p-3"
+            >
+              Save
+            </button>
+          )}
         </form>
       </Modal>
       {/*  */}
@@ -154,8 +172,14 @@ export default function Courses() {
       {/* Side nav */}
       {/* Main View */}
       {/* Form to add course */}
-     
-      <div onClick={(e) => openModal()} className="dashboardCards_add">
+
+      <div
+        onClick={(e) => {
+          setAddORMod(true);
+          openModal();
+        }}
+        className="dashboardCards_add"
+      >
         <svg width="15" height="15" viewBox="0 0 15 15">
           <path
             d="M7.5 0L7.5 15M0 7.5L15 7.5"
@@ -169,88 +193,90 @@ export default function Courses() {
       {/* Form to add course */}
 
       {/* Table to show courses */}
-      
-         { Loading ? ( <>
+
+      {Loading ? (
+        <>
           <div id="loading">
-      <div id="load">
-        <div>G</div>
-        <div>N</div>
-        <div>I</div>
-        <div>D</div>
-        <div>A</div>
-        <div>O</div>
-        <div>L</div>
-      </div>
-    </div>
-    <div className="col-span-4 row-span-10 border m-5 overflow-y-scroll overflow-x-scroll relative">
-    <table className="w-full ">
-      <thead className="theader">
-        <tr>
-          <th>ID</th>
-          <th>Course Name</th>
-          <th>Description</th>
-          <th>Instructor ID</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="hidden">
-
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        </tr>
-            </tbody>
-        </table>
-        </div>
-
+            <div id="load">
+              <div>G</div>
+              <div>N</div>
+              <div>I</div>
+              <div>D</div>
+              <div>A</div>
+              <div>O</div>
+              <div>L</div>
+            </div>
+          </div>
+          <div className="col-span-4 row-span-10 border m-5 overflow-y-scroll overflow-x-scroll relative">
+            <table className="w-full ">
+              <thead className="theader">
+                <tr>
+                  <th>ID</th>
+                  <th>Course Name</th>
+                  <th>Description</th>
+                  <th>Instructor ID</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="hidden">
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </>
-  ) : (
-    <div className="col-span-4 row-span-10 border m-5 overflow-y-scroll overflow-x-scroll relative">
-        <table className="w-full ">
-          <thead className="theader">
-            <tr>
-          <th>ID</th>
-          <th>Course Name</th>
-          <th>Description</th>
-          <th>Instructor ID</th>
-          <th>Action</th>
-        </tr>
-          </thead>
-          <tbody>
-               { Courses.map((user) => (
+      ) : (
+        <div className="col-span-4 row-span-10 border m-5 overflow-y-scroll overflow-x-scroll relative">
+          <table className="w-full ">
+            <thead className="theader">
               <tr>
-                <td>{user._id}</td>
-                <td>{user.course_name}</td>
-                <td>{user.description}</td>
-                <td>{user.instructor}</td>
-                <td className="p-1 flex justify-around">
-                  <button
-                    onClick={(e) => modifyCourse(e)}
-                    className="p-2 text-firstBlue border border-firstBlue rounded-full font-bold "
-                  >
-                    Modify Course
-                  </button>
-                  <button
-                    onClick={(e) => removeCourse(e)}
-                    className="p-2 text-red-500 border border-red-500 rounded-full font-bold "
-                  >
-                    Delete Course
-                  </button>
-                </td>
+                <th>ID</th>
+                <th>Course Name</th>
+                <th>Description</th>
+                <th>Instructor ID</th>
+                <th>Action</th>
               </tr>
-        ))}
+            </thead>
+            <tbody>
+              {Courses.map((user) => (
+                <tr>
+                  <td>{user._id}</td>
+                  <td>{user.course_name}</td>
+                  <td>{user.description}</td>
+                  <td>{user.instructor}</td>
+                  <td className="p-1 flex justify-around">
+                    <button
+                      onClick={(e) => {
+                        setAddORMod(false);
+                        modifyCourse(e);
+                      }}
+                      className="p-2 text-firstBlue border border-firstBlue rounded-full font-bold "
+                    >
+                      Modify Course
+                    </button>
+                    <button
+                      onClick={(e) => removeCourse(e)}
+                      className="p-2 text-red-500 border border-red-500 rounded-full font-bold "
+                    >
+                      Delete Course
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
-        </table>
-      </div>
-            )}
+          </table>
+        </div>
+      )}
       {/* Table to show courses */}
       {/* Main View */}
       {/* Footer View */}
       <Copyright />
       {/* Footer View */}
     </div>
-  )
+  );
 }
--- src/app/api/courses/[id]/route.ts
+++ src/app/api/courses/[id]/route.ts
@@ -6,11 +6,11 @@ import connectDB from "@/database/lib/mongodb";
 // PUT update one course by id
 export async function PUT(request:NextRequest,{params}:{params:{id:string}}){
     const {id} = params
-    const { new_course_name,new_description,new_instructor} =await request.json()
+    const { course_name,description,instructor} =await request.json()
     
     try {
         await connectDB()
-        const res = await Courses.findByIdAndUpdate(id,{course_name : new_course_name , description : new_description , instructor : new_instructor})
+        const res = await Courses.findByIdAndUpdate(id,{course_name , description  , instructor })
         return NextResponse.json({message : "Course updated successfuly" , data : res},{status : 201})
     } catch (error) {
         return NextResponse.json({message : "No course updated" , error:error})
