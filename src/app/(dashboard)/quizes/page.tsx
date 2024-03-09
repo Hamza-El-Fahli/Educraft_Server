91467d0d893df7a1a435174957fa67ed92e643ce
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-03-09 15:40:01 +0100

    wip: Quizes Management
    - create ShowQuizes
    - add some styling

--- src/app/(dashboard)/quizes/page.tsx
+++ src/app/(dashboard)/quizes/page.tsx
@@ -1,27 +1,13 @@
-export default function Quizes(){
+"use client"
 
 
-
+import ShowQuizes from "@/components/ShowQuizes";
+export default function Quizes() {
     let Data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-    return( 
-    
-    <div className=" col-span-4">
-    <div className=" h-10 " />
-{Data.map((quiz , index)=>{
-    return (
-        <div className="border rounded-t-md h-10 hover:h-60 duration-500 px-5 py-1 m-0 overflow-hidden bg-secondary" key={index}>
-        <div className="h-10 p-0 m-0">My Quiz Data {quiz}</div>
-        <div className="h-10 p-0 m-0">Answer</div>
-        <div className="h-10 p-0 m-0">Answer</div>
-        <div className="h-10 p-0 m-0">Answer</div>
-        <div className="h-10 p-0 m-0">Answer</div>
-        <div className="h-10 p-0 m-0">Correct Answer</div>
-        </div>
-        
-    )
-})}
-   
-   
-    
-    </div>)
+
+  return (
+    <div className="col-span-4">
+        <ShowQuizes Data={Data} />
+    </div>
+  );
 }
---
+++ src/components/ShowQuizes.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+
+import { useState } from "react";
+
+export default function ShowQuizes({Data}:{Data : number[]}){
+    
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (index:any) => {
+    setExpandedIndex((prevIndex) => (prevIndex === index ? -1 : index));
+  };
+
+   return  <>
+
+<div className="border h-20"></div>
+
+      {Data.map((quiz, index) => {
+        const isExpanded = expandedIndex === index;
+        return (
+          <div
+            key={index}
+            className={`border border-2 rounded-md ${isExpanded ? "h-60" : "h-10"} duration-500 p-0 m-2 overflow-hidden bg-secondary`}
+          >
+            <div className="h-10 p-0 m-0 flex items-center px-5 bg-third"             
+            onClick={() => handleClick(index)}
+>My Quiz Data {quiz}</div>
+              <>
+              <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-200  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+              <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-150  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+              <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-100  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+              <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-75  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+              <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-0  h-10 p-0 m-0 flex items-center px-5 `}>Correct</div>
+              </>
+            
+          </div>
+        );
+      })}
+      </>
+}
