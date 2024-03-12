1773d13dafd8476a227a45caf33d75ff6a3d6586
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-03-12 01:23:22 +0100

    wip: Quizzes Management
    - make the quizzes two collumns

--- src/components/ShowQuizzes.tsx
+++ src/components/ShowQuizzes.tsx
@@ -11,28 +11,42 @@ export default function ShowQuizes({Data}:{Data : number[]}){
     setExpandedIndex((prevIndex) => (prevIndex === index ? -1 : index));
   };
 
-   return  <>
+   return  <div className="grid grid-cols-3" style={{gridAutoRows : '50px'}}>
 
       {Data.map((quiz, index) => {
         const isExpanded = expandedIndex === index;
         return (
-          <div
+//           <div
+//             key={index}
+//             className={`border-2 rounded-md ${isExpanded ? "h-60" : "h-10"} duration-500 p-0 m-2 overflow-hidden bg-secondary`}
+//           >
+//             <div className="h-10 p-0 m-0 flex items-center px-5 bg-blue-950"             
+//             onClick={() => handleClick(index)}
+// >Quiz N {quiz}</div>
+//               <>
+//               <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-200  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+//               <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-150  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+//               <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-100  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+//               <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-75  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+//               <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-0  h-10 p-0 m-0 flex items-center px-5 `}>Correct</div>
+//               </>
+            
+//           </div>
+<div
             key={index}
-            className={`border-2 rounded-md ${isExpanded ? "h-60" : "h-10"} duration-500 p-0 m-2 overflow-hidden bg-secondary`}
-          >
+           className={`border-2 rounded-md h-10 duration-500 p-0 m-2 overflow-hidden bg-secondary z-0 hover:h-60 quizHovering`}            >
             <div className="h-10 p-0 m-0 flex items-center px-5 bg-blue-950"             
-            onClick={() => handleClick(index)}
->My Quiz Data {quiz}</div>
+>Quiz N {quiz}</div>
               <>
-              <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-200  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
-              <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-150  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
-              <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-100  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
-              <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-75  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
-              <div className={` bg-secondary ${isExpanded ? 'opacity-1' : 'opacity-0' } duration-0  h-10 p-0 m-0 flex items-center px-5 `}>Correct</div>
+              <div className={` bg-secondary duration-200  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+              <div className={` bg-secondary duration-150  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+              <div className={` bg-secondary duration-100  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+              <div className={` bg-secondary duration-75  h-10 p-0 m-0 flex items-center px-5 `}>Answer</div>
+              <div className={` bg-secondary duration-0  h-10 p-0 m-0 flex items-center px-5 `}>Correct</div>
               </>
             
           </div>
         );
       })}
-      </>
+      </div>
 }
