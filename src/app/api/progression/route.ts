8f9fc77cdcebdb85c5186879521fcc3b1c2f9247
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-04-02 02:30:43 +0000

    wip: Module Progression API  - create module progression API

--- src/app/api/progression/route.ts
+++ src/app/api/progression/route.ts
@@ -1,11 +1,20 @@
-import { AddChapterProgress } from "@/controllers/progressionController";
-import { NextRequest } from "next/server";
+import { AddChapterProgress, AddModuleProgress } from "@/controllers/progressionController";
+import { NextRequest, NextResponse } from "next/server";
 
 
 
 
 export async function  POST(request:NextRequest) {
 
-    return await AddChapterProgress(request)
+    const {user_id , chapter_id , score , module_id} = await request.json()
+
+
+    if(chapter_id)
+    return await AddChapterProgress({user_id , chapter_id , score})
+    if(module_id)
+    return await AddModuleProgress({user_id , module_id , score})
+
+    return NextResponse.json({error : 'Didnt specify module or chapter'})
+
     
 }
--- src/controllers/progressionController.ts
+++ src/controllers/progressionController.ts
@@ -1,26 +1,36 @@
 import connectDB from "@/database/lib/mongodb"
 import ChaptersProgression from "@/database/models/chaptersProgression"
+import ModulesProgression from "@/database/models/modulesProgression"
 import { NextRequest, NextResponse } from "next/server"
 
-export async function AddChapterProgress(request:NextRequest){
-
+export async function AddChapterProgress({ user_id, chapter_id, score }: { user_id: string, chapter_id: string, score?: string }) {
     await connectDB()
-    
-    const {user_id , chapter_id , score} = await request.json()
-    const currnetScore = await  ChaptersProgression.findOne({user_id , chapter_id})
-    if(currnetScore ){
-        if(score && score > currnetScore.score){
-            await ChaptersProgression.findOneAndUpdate( currnetScore._id, {score})
-            return NextResponse.json({message:'Chapter user score updated successfully'})
+    const currnetScore = await ChaptersProgression.findOne({ user_id, chapter_id })
+    if (currnetScore) {
+        if (score && score > currnetScore.score) {
+            await ChaptersProgression.findOneAndUpdate(currnetScore._id, { score })
+            return NextResponse.json({ message: 'Chapter user score updated successfully' })
         }
-        return NextResponse.json({message:'Chapter user score didnt changed'})
+        return NextResponse.json({ message: 'Chapter user score didnt changed' })
     }
-    else{
-        await ChaptersProgression.create({user_id , chapter_id , score : score || 0 })
-        return NextResponse.json({message:'NEW Chapter user score Create successfully'})
-
+    else 
+    {
+        await ChaptersProgression.create({ user_id, chapter_id, score: score || 0 })
+        return NextResponse.json({ message: 'NEW Chapter user score Create successfully' })
     }
-    
-
-        
+}
+export async function AddModuleProgress({ user_id, module_id, score }: { user_id: string, module_id: string, score?: string }) {
+    await connectDB()
+    const currnetScore = await ModulesProgression.findOne({ user_id, module_id })
+    if (currnetScore) {
+        if (score && score > currnetScore.score) {
+            await ModulesProgression.findOneAndUpdate(currnetScore._id, { score })
+            return NextResponse.json({ message: 'Module user score updated successfully' })
+        }
+        return NextResponse.json({ message: 'Module user score didnt changed' })
+    }
+    else {
+        await ModulesProgression.create({ user_id, module_id, score: score || 0 })
+        return NextResponse.json({ message: 'NEW Module user score Create successfully' })
+    }
 }
