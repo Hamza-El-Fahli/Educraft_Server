b3bcd49caafdc413e0cfc10b11886c703b1faaa7
hamza-el-fahli <hamzaelfahli4@gmail.com>
2024-04-10 14:54:04 +0000

    wip: Progression API - count passed chapters - responce with number of passed chapter and total chapters in module

--- src/app/api/progression/quizProgress/route.ts
+++ src/app/api/progression/quizProgress/route.ts
@@ -1,9 +1,21 @@
+import { GetChaptersWithModuleID } from "@/controllers/chaptersControlllers";
 import { GetUserProgress , getQuizGroupsByChapter } from "@/controllers/progressionController";
+import connectDB from "@/database/lib/mongodb";
+import Chapters from "@/database/models/chapters";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request:NextRequest){
+    await connectDB()
+
     const {user_id, module_id} = await request.json()
-    const data =await getQuizGroupsByChapter({user_id,module_id})
+    const passedQuizzes =await getQuizGroupsByChapter({user_id,module_id})
+    const chapterUnderTheModule =await Chapters.find({module_id: "65e9cbaedc6606713a973fb0"})
+    let numberOfPassedChapters = 0
+    chapterUnderTheModule.map((chapter)=>{
+        if(passedQuizzes[chapter._id])
+            if( chapter.quizGroupes == passedQuizzes[chapter._id].length )
+                numberOfPassedChapters += 1
+            })
     
-    return NextResponse.json(data)
+    return NextResponse.json({ numberOfPassedChapters , totalOfChapters  : chapterUnderTheModule.length})
 }
