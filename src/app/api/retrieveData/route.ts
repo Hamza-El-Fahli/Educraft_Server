0e8bfe1530608a60b4c3fd23f3086a8fb03dff4f
Hamza-El-Fahli <hamzaelfahli4@gmail.com>
2024-05-24 01:17:22 +0100

    wip: RetrieveData API - optimise code and manages errors

--- src/app/api/retrieveData/route.ts
+++ src/app/api/retrieveData/route.ts
@@ -5,15 +5,39 @@ import Modules from "@/database/models/modules";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const conn = await pool.getConnection();
 
     const courses = request.nextUrl.searchParams.get('courses')
     const modules = request.nextUrl.searchParams.get('modules')
     const chapters = request.nextUrl.searchParams.get('chapters')
     const res: any = {};
-    if (courses == '1') res.courses = await conn.query('SELECT * FROM courses');
-    if (modules == '1') res.modules = await conn.query('SELECT * FROM modules');
-    if (chapters == '1') res.chapters = await conn.query('SELECT * FROM chapters');
-    conn.release();
-    return NextResponse.json(res)
+    if(courses || modules || chapters){
+        let queries = [];
+        if (courses == '1') queries.push('SELECT * FROM courses');
+        if (modules == '1') queries.push('SELECT * FROM modules');
+        if (chapters == '1') queries.push('SELECT * FROM chapters');
+    
+        const combinedQuery = queries.join('; ');
+    
+        try {
+            const conn = await pool.getConnection();
+
+            const results = await conn.query(combinedQuery);
+            if(Array.isArray(results[0])){
+            if (courses == '1') res.courses = results.shift();
+            if (modules == '1') res.modules = results.shift();
+            if (chapters == '1') res.chapters = results.shift();}
+            else
+                res.courses = results
+            conn.release();
+            return NextResponse.json(res)
+        } catch (error) {
+            console.error('Database query error:', error);
+            return NextResponse.json({error:"Database Error"},{status:404})
+
+        }
+}
+    else{
+        return NextResponse.json({error:null},{status:404})
+
+    }
 }
