27db7e821517282c9575356fe877461cb18e4a18
Hamza-El-Fahli <hamzaelfahli4@gmail.com>
2024-06-05 20:24:17 +0100

    wip: UserPlatform - create first page template

---
+++ src/app/userPlatform/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+<header className=" bg-primary bg- h-16 w-full flex px-36 text-white justify-between items-center ">
+<span>logo</span>
+<span>User</span>
+            </header>  )
+}
---
+++ src/app/userPlatform/courses/page.tsx
@@ -0,0 +1,37 @@
+import { Header } from "../components/Header";
+
+export default function User_courses() {
+    return (
+        <div className=" h-svh w-full flex flex-col" >
+            <Header/>
+            <main className="w-full h-full  ">
+            <div className="w-full flex gap-10 text-white pt-10 bg-secondary/60">
+                <nav className="bg-[#1F233A] w-auto px-10 py-2 flex justify-center items-center rounded-t-full">Reaseax Informatiques</nav>
+                <nav className="bg-[#1F233A] w-auto px-10 py-2 flex justify-center items-center rounded-t-full">SE Open Source</nav>
+                <nav className="bg-[#1F233A] w-auto px-10 py-2 flex justify-center items-center rounded-t-full">Gestion Des Bases De donnees</nav>
+            </div>
+            <div className="h-full flex flex-col space-y-8 px-10 pt-10 ">
+                <div><h1>CCNA</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam recusandae voluptatem error repellendus ut aspernatur. Voluptatibus, modi? Doloribus, maiores minima facilis quidem distinctio ullam soluta, quasi illum, placeat vero eaque!</p></div>
+                <div>user courses:</div>
+                <div className="grid grid-flow-row grid-cols-3 gap-4">
+                <div className="p-5 h-56 bg-[#21233B]/60"></div>
+                <div className="p-5 h-56 bg-[#21233B]/60"></div>
+                <div className="p-5 h-56 bg-[#21233B]/60"></div>
+                <div className="p-5 h-56 bg-[#21233B]/60"></div>
+                <div className="p-5 h-56 bg-[#21233B]/60"></div>
+                <div className="p-5 h-56 bg-[#21233B]/60"></div>
+                </div>
+            </div>
+
+            </main>
+            <footer className="h-32">
+                This is footer
+            </footer>
+
+
+
+
+
+        </div>
+    )
+}
