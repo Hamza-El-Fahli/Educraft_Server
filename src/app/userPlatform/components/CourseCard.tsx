c4df881df14c0ae6d57b2bc70e5ab7273190936a
Hamza-El-Fahli <hamzaelfahli4@gmail.com>
2024-06-05 21:23:32 +0100

    wip: UserPlatform - courses page structured

--- src/app/userPlatform/components/CourseCard.tsx
+++ src/app/userPlatform/components/CourseCard.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function CourseCard() {
   return (
     
-    <div className="p-3 pb-2 h-64 bg-[#21233B]/70 border-[#0090BD]/60 border-2 rounded-xl flex flex-col gap-2 text-white">
-    <div className="border h-1/2 w-full rounded-xl">Image</div>
+    <div className="p-3 pb-2 h-64 bg-[#21233B]/50 border-[#0090BD]/20 border-2 rounded-xl flex flex-col gap-2 text-white">
+    <img src='/image_1.png' className="h-1/2 w-full " />
     <h2 className="font-bold">CCNA 1</h2>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
     <button className="bg-firstBlue p-1 rounded-xl font-bold">ENROLL</button>
--- src/app/userPlatform/courses/page.tsx
+++ src/app/userPlatform/courses/page.tsx
@@ -3,21 +3,21 @@ import { Header } from "../components/Header";
 
 export default function User_courses() {
     return (
-        <div className=" h-svh w-full flex  flex-col " style={{background:'url(/networking-bg.png)'}} >
+        <div className=" h-svh w-full flex  flex-col " style={{ background: 'url(/networking-bg.png)' }} >
             <Header />
-            <main className="w-full h-full  ">
-                <div className="w-full flex gap-10 text-white pt-10 bg-secondary/50">
-                    <nav className="bg-[#1F233A] w-auto px-10 py-2 flex justify-center items-center rounded-t-full">Reaseax Informatiques</nav>
-                    <nav className="bg-[#1F233A] w-auto px-10 py-2 flex justify-center items-center rounded-t-full">SE Open Source</nav>
-                    <nav className="bg-[#1F233A] w-auto px-10 py-2 flex justify-center items-center rounded-t-full">Gestion Des Bases De donnees</nav>
+            <main className="w-full h-full">
+                <div className="w-full flex gap-10 text-white pt-10 pl-5 bg-secondary/50 " style={{ borderBottomWidth: 1 }}>
+                    <nav className="bg-[#1F233A] w-auto px-10 py-2 flex justify-center items-center rounded-t-full border-b-4 border-firstBlue " style={{ marginBottom: -1 }}>Reaseax Informatiques</nav>
+                    <nav className="bg-[#1F233A] w-auto px-10 py-2 flex justify-center items-center rounded-t-full" style={{ marginBottom: -1 }}>SE Open Source</nav>
+                    <nav className="bg-[#1F233A] w-auto px-10 py-2 flex justify-center items-center rounded-t-full" style={{ marginBottom: -1 }}>Gestion Des Bases De donnees</nav>
                 </div>
                 <div className="h-full flex flex-col space-y-8 px-10 pt-10  bg-secondary/50 text-white">
-                    <div><h1>Reaseax Informatiques</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam recusandae voluptatem error repellendus ut aspernatur. Voluptatibus, modi? Doloribus, maiores minima facilis quidem distinctio ullam soluta, quasi illum, placeat vero eaque!</p>
+                    <div><h1 className="font-bold text-2xl">Reaseax Informatiques</h1>
+                        <p className="font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam recusandae voluptatem error repellendus ut aspernatur. Voluptatibus, modi? Doloribus, maiores minima facilis quidem distinctio ullam soluta, quasi illum, placeat vero eaque!</p>
                     </div>
-                    <div>user courses:</div>
+                    <h1 className="font-bold text-2xl">user courses:</h1>
                     <div className="grid grid-flow-row md:grid-cols-3 xl:grid-cols-6 gap-4">
-                        
+
                         <CourseCard />
                         <CourseCard />
                         <CourseCard />
