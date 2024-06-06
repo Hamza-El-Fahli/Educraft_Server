bb4c370020723b5fc87b860f7e5596c3082f1eab
Hamza-El-Fahli <hamzaelfahli4@gmail.com>
2024-06-06 14:19:05 +0100

    wip: UserPlatform - add button to open chapters

--- src/app/userPlatform/components/ModuleCard.tsx
+++ src/app/userPlatform/components/ModuleCard.tsx
@@ -1,11 +1,12 @@
-import { API_Server_Chapters } from '@/configuration/API'
+import { API_Server_Chapters, API_Storage_Server } from '@/configuration/API'
 import { IChapter, IModule } from '@/types/types'
 import React, { useEffect, useRef, useState } from 'react'
 
 function ModuleCard({ _module, user_id }: { _module: IModule, user_id: string }) {
+    const [IsChaptersOpened, setIsChaptersOpened] = useState(false)
     const [Chapters, setChapters] = useState([])
     const chapters = useRef(null)
-const [Height, setHeight] = useState(10)
+const [Height, setHeight] = useState(0)
     useEffect(() => {
         fetch(`${API_Server_Chapters}?module_id=${_module._id}&user_id=${user_id}`)
             .then(async (res) => {
@@ -26,10 +27,17 @@ const [Height, setHeight] = useState(10)
     }
 
     function itemNotFocused(){
-        setHeight(1)
+        setHeight(0)
 
     }
-
+    function HandleClick(){
+        if(IsChaptersOpened){
+            itemNotFocused()
+        }else{
+            itemFocused()
+        }
+        setIsChaptersOpened(!IsChaptersOpened)
+    }
  
 
     return (
@@ -41,13 +49,13 @@ const [Height, setHeight] = useState(10)
                 </span>
                 <span className='self-center text-firstBlue'>
 
-                    <button className='line p-3 pb-4 text-firstBlue rotate-180 origin-center  duration-500 text-2xl hover:rotate-0'>&#10699;</button>
+                    <button className='line p-3 pb-4 text-firstBlue rotate-180 origin-center  duration-500 text-2xl hover:rotate-0' onClick={()=>HandleClick()}>&#10699;</button>
                 </span>
 
             </div>
-            <div ref={chapters} className=' w-full overflow-hidden duration-500' style={{height:Height}} onMouseEnter={()=>itemFocused()} onMouseLeave={()=>{itemNotFocused()}}>
+            <div ref={chapters} className=' w-full overflow-hidden duration-500' style={{height:Height}}>
                 
-                {Chapters.length > 0 &&
+                {Chapters.length > 0 ?
                     Chapters.map((chapter: IChapter, index: number) => {
                         return (
                             <div key={index} className='px-3 border-b flex justify-between  bg-gray-900/80  '>
@@ -55,7 +63,8 @@ const [Height, setHeight] = useState(10)
                                 <a href="#" className='text-firstBlue w-44 text-right'>View chapter</a>
                             </div>
                         )
-                    })
+                    }) :
+                    <div>No Chapters Here</div>
                 }
             </div>
         </div>
