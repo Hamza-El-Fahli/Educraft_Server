808278f30a4bbb7debee3c11a540fafcae791b35
Hamza-El-Fahli <hamzaelfahli4@gmail.com>
2024-06-10 15:14:16 +0100

    fur: Users API - throw error No user is found

--- src/database/models/users.ts
+++ src/database/models/users.ts
@@ -13,7 +13,7 @@
 //     profile: Profile;
 //     annee: Profile;
 //     filiere: Profile;
-// } 
+// }
 
 // const UserSchema: Schema = new Schema({
 //     name: { type: String, required: true },
@@ -32,102 +32,141 @@
 import pool from "./../lib/mariadb";
 
 const Users: any = {};
-Users.findOne = async (data: { name?: string, email?: string }) => {
-    let nameOrEmail = data.name ? data.name : data.email;
+Users.findOne = async (data: { name?: string; email?: string }) => {
+  let nameOrEmail = data.name ? data.name : data.email;
 
-    const query = `SELECT * FROM users WHERE name = ? OR email = ? LIMIT 1`;
-    const lastActivityQuery = `SELECT * FROM progression WHERE user_id = ? ORDER BY createdAt DESC LIMIT 1`;
+  const query = `SELECT * FROM users WHERE name = ? OR email = ? LIMIT 1`;
+  const lastActivityQuery = `SELECT * FROM progression WHERE user_id = ? ORDER BY createdAt DESC LIMIT 1`;
 
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        const rows = await conn.query(query, [nameOrEmail, nameOrEmail]);
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query(query, [nameOrEmail, nameOrEmail]);
+    if (rows.length == 0) throw Error("No user is found");
+    const lastActivity = await conn.query(lastActivityQuery, [rows[0]._id]);
+    conn.release(); // Release the connection after executing all queries
 
-        const lastActivity = await conn.query(lastActivityQuery,[rows[0]._id]);
-        conn.release(); // Release the connection after executing all queries
+    if (lastActivity.length != 0)
+      rows[0].lastActivity = lastActivity[0].createdAt;
+    else rows[0].lastActivity = rows[0].createdAt;
 
-        if (lastActivity.length != 0)
-            rows[0].lastActivity = lastActivity[0].createdAt;
-        else
-            rows[0].lastActivity = rows[0].createdAt;
-        
-        return rows[0];
-    } catch (error) {
-        console.error('Error:', error);
-        if (conn) conn.release(); // Make sure to release the connection in case of an error
-        return null;
-    }
-}
+    return rows[0];
+  } catch (error) {
+    console.error("Error:", error);
+    if (conn) conn.release(); // Make sure to release the connection in case of an error
+    return null;
+  }
+};
 
 Users.find = async (data: null | any) => {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        let rows;
-        if (data == null)
-            rows = await conn.query('SELECT * FROM users ;');
-        else    
-            rows = await conn.query('SELECT * FROM users WHERE _id != ?;',[data]);
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    let rows;
+    if (data == null) rows = await conn.query("SELECT * FROM users ;");
+    else rows = await conn.query("SELECT * FROM users WHERE _id != ?;", [data]);
 
-        conn.release();
-        return rows
-    } catch (error) {
-        console.error('Error:', error);
-        return null
-    }
-}
+    conn.release();
+    return rows;
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
 
-Users.create = async ({ name, email, password, annee, filiere, profile }: { name: string, email: string, password: string, annee: number, filiere: string, profile: string }) => {
-    let conn
-    const query = `INSERT INTO users (name, email, password, annee, filiere, profile) VALUES (?, ?, ?, ?, ?, ?);`;
-    try {
-        conn = await pool.getConnection();
-        let rows;
-        rows = await conn.query(query,[name, email, password, annee, filiere, profile]);
-        conn.release();
-        return { _id: parseInt(rows.insertId) }
-    } catch (error) {
-        console.error('Error:', error);
-        return null
-    }
-}
-Users.findByIdAndUpdate = async (id: string, { name, email, password, annee, filiere, profile }: { name: string, email: string, password: string, annee: number, filiere: string, profile: string }) => {
-    let conn;
-    const query = `UPDATE users
+Users.create = async ({
+  name,
+  email,
+  password,
+  annee,
+  filiere,
+  profile,
+}: {
+  name: string;
+  email: string;
+  password: string;
+  annee: number;
+  filiere: string;
+  profile: string;
+}) => {
+  let conn;
+  const query = `INSERT INTO users (name, email, password, annee, filiere, profile) VALUES (?, ?, ?, ?, ?, ?);`;
+  try {
+    conn = await pool.getConnection();
+    let rows;
+    rows = await conn.query(query, [
+      name,
+      email,
+      password,
+      annee,
+      filiere,
+      profile,
+    ]);
+    conn.release();
+    return { _id: parseInt(rows.insertId) };
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
+Users.findByIdAndUpdate = async (
+  id: string,
+  {
+    name,
+    email,
+    password,
+    annee,
+    filiere,
+    profile,
+  }: {
+    name: string;
+    email: string;
+    password: string;
+    annee: number;
+    filiere: string;
+    profile: string;
+  }
+) => {
+  let conn;
+  const query = `UPDATE users
     SET name = ?, email = ?, password = ?, annee = ?, filiere = ?, profile = ?
     WHERE _id = ?;`;
 
-    try {
-        conn = await pool.getConnection();
-        const rows = await conn.query(query, [name, email, password, annee, filiere, profile, id]);        conn.release();
-        if (rows.affectedRows > 0)
-            return rows
-        else
-            return null
-    } catch (error) {
-        console.error('Error:', error);
-        return null
-    }
-}
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query(query, [
+      name,
+      email,
+      password,
+      annee,
+      filiere,
+      profile,
+      id,
+    ]);
+    conn.release();
+    if (rows.affectedRows > 0) return rows;
+    else return null;
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
 
 Users.findByIdAndDelete = async (id: string) => {
-    let conn;
-    const query = `DELETE FROM users    WHERE _id = ?;`;
+  let conn;
+  const query = `DELETE FROM users    WHERE _id = ?;`;
 
-    try {
-        conn = await pool.getConnection();
-        let rows;
-        rows = await conn.query(query,[id]);
-        conn.release();
-        if (rows.affectedRows > 0)
-            return rows
-        else
-            return null
-    } catch (error) {
-        console.error('Error:', error);
-        return null
-    }
-}
+  try {
+    conn = await pool.getConnection();
+    let rows;
+    rows = await conn.query(query, [id]);
+    conn.release();
+    if (rows.affectedRows > 0) return rows;
+    else return null;
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
 
-
-export default Users
+export default Users;
