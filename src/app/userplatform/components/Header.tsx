62a198f9f4e7b6df22467fbd07de0afbe017a46a
Hamza-El-Fahli <hamzaelfahli4@gmail.com>
2024-07-10 13:18:19 +0100

    wip: userPlatform - add link to logo - display username

--- src/app/userplatform/components/Header.tsx
+++ src/app/userplatform/components/Header.tsx
@@ -1,10 +1,13 @@
-"use client"
-import React from 'react'
-import '@/styles/dropdown.css'
-import { useRouter } from 'next/navigation'
-import { useLogout } from '@/hooks/useLogout'
+"use client";
+import React from "react";
+import "@/styles/dropdown.css";
+import { useRouter } from "next/navigation";
+import { useLogout } from "@/hooks/useLogout";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
 export const Header = () => {
-  const router = useRouter()
+  const user = useCurrentUser();
+
+  const router = useRouter();
   const logoutHook = useLogout();
   const logout = () => {
     logoutHook.logout();
@@ -12,19 +15,36 @@ export const Header = () => {
   };
   return (
     <header className="sticky top-0 bg-primary bg- w-full flex px-36 py-2 text-white justify-between items-center z-10 ">
-  <button className='absolute left-5 text-white text-2xl' onClick={()=>router.back()}>&#60;</button>
-<img src='/WhiteLogo.png' className='h-8' />
+      <button
+        className="absolute left-5 text-white text-2xl"
+        onClick={() => router.back()}
+      >
+        &#60;
+      </button>
+      <a href="/">
+        <img src="/WhiteLogo.png" className="h-8" />
+      </a>
 
       <nav className="sub-menu-parent" tab-index="0">
-        <span className='flex items-center gap-3'>
-          <span className='text-left text-md font-bold'>UserNAME<br /><h6 className='text-xs'>Two lines</h6></span>
-          <img src='/avatars/M_1.jpg' className='h-8 rounded-full ' />
-
-        </span>       
-         <ul className="sub-menu">
-          <li className=''><a href="#">Parametres</a></li>
-          <li><a href="#" onClick={(e) => logout()} >Logout</a></li>
+        <span className="flex items-center gap-3">
+          <span className="text-left text-md font-bold">
+            {user?.name}
+            <br />
+            <h6 className="text-xs">{/* Two lines */}</h6>
+          </span>
+          <img src="/avatars/M_1.jpg" className="h-8 rounded-full " />
+        </span>
+        <ul className="sub-menu">
+          <li className="">
+            <a href="#">Parametres</a>
+          </li>
+          <li>
+            <a href="#" onClick={(e) => logout()}>
+              Logout
+            </a>
+          </li>
         </ul>
       </nav>
-    </header>)
-}
+    </header>
+  );
+};
