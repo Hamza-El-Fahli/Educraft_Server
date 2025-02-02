82caf32e51c7bf0a60ee372cf6add0ffd85c8fd2
hamza-el-fahli <hamzeelfahli4@gmail.com>
2025-02-02 10:29:18 -0600

    wip: Security Story - move all sensitive data to .env

--- src/database/lib/mariadb.js
+++ src/database/lib/mariadb.js
@@ -1,17 +1,22 @@
 import mariadb from "mariadb";
+import dotenv from "dotenv";
+
+// Load environment variables from .env.local
+dotenv.config();
 
 const pool = mariadb.createPool({
-  host: "localhost", // db.educraft.com
-  user: "educraft",
-  // port:11529,
-  password: "9c5e28dc5cf27969",
-  database: "educraft",
-  connectionLimit: 5,
-  connectTimeout: 20000, // Increase the connection timeout to 20 seconds
-  multipleStatements: true, // Enable multiple statements
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT
+    ? parseInt(process.env.DB_CONNECTION_LIMIT)
+    : 5,
+  connectTimeout: process.env.DB_CONNECT_TIMEOUT
+    ? parseInt(process.env.DB_CONNECT_TIMEOUT)
+    : 20000,
+  multipleStatements: true,
 });
-
-// run ngrok using
-//  ngrok tcp 172.17.0.2:3306
 
 export default pool;
